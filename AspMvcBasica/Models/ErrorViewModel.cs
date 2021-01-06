17bf0a5123fdfadd54483c802ae1c815db93d5db
Gabriel Coronin <gabrielcoronin@gmail.com>
2021-01-06 11:03:25 -0300

    Add project files.

---
+++ AspMvcBasica/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspMvcBasica.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
